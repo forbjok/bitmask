50771cefbd0d339f14e92a62bdbd8dc66441bcf5
Forb.Jok <forbjok@gmail.com>
2017-08-17 19:18:50 +0200

    refactor: Use type alias in benchmark

--- BitMask/Benchmark/BenchmarkImplementation.cs
+++ BitMask/Benchmark/BenchmarkImplementation.cs
@@ -8,81 +8,83 @@ using BitMasks;
 
 namespace Benchmark
 {
+    using IntType = Int64;
+
     public class BenchmarkImplementation
     {
-        public enum ComponentType : long
+        public enum ComponentType : IntType
         {
             None = 0,
-            Component1 = 1 << 0,
-            Component2 = 1 << 1,
-            Component3 = 1 << 2,
-            Component4 = 1 << 3,
-            Component5 = 1 << 4,
-            Component6 = 1 << 5,
-            Component7 = 1 << 6,
-            Component8 = 1 << 7,
-            Component9 = 1 << 8,
-            Component10 = 1 << 9,
-            Component11 = 1 << 10,
-            Component12 = 1 << 11,
-            Component13 = 1 << 12,
-            Component14 = 1 << 13,
-            Component15 = 1 << 14,
-            Component16 = 1 << 15,
-            Component17 = 1 << 16,
-            Component18 = 1 << 17,
-            Component19 = 1 << 18,
-            Component20 = 1 << 19,
-            Component21 = 1 << 20,
-            Component22 = 1 << 21,
-            Component23 = 1 << 22,
-            Component24 = 1 << 23,
-            Component25 = 1 << 24,
-            Component26 = 1 << 25,
-            Component27 = 1 << 26,
-            Component28 = 1 << 27,
-            Component29 = 1 << 28,
-            Component30 = 1 << 29,
-            Component31 = 1 << 30,
-            Component32 = 1 << 31,
-            Component33 = 1 << 32,
-            Component34 = 1 << 33,
-            Component35 = 1 << 34,
-            Component36 = 1 << 35,
-            Component37 = 1 << 36,
-            Component38 = 1 << 37,
-            Component39 = 1 << 38,
-            Component40 = 1 << 39,
-            Component41 = 1 << 40,
-            Component42 = 1 << 41,
-            Component43 = 1 << 42,
-            Component44 = 1 << 43,
-            Component45 = 1 << 44,
-            Component46 = 1 << 45,
-            Component47 = 1 << 46,
-            Component48 = 1 << 47,
-            Component49 = 1 << 48,
-            Component50 = 1 << 49,
-            Component51 = 1 << 50,
-            Component52 = 1 << 51,
-            Component53 = 1 << 52,
-            Component54 = 1 << 53,
-            Component55 = 1 << 54,
-            Component56 = 1 << 55,
-            Component57 = 1 << 56,
-            Component58 = 1 << 57,
-            Component59 = 1 << 58,
-            Component60 = 1 << 59,
-            Component61 = 1 << 60,
-            Component62 = 1 << 61,
-            Component63 = 1 << 62,
-            Component64 = 1 << 63,
+            Component1 = (IntType) 1 << 0,
+            Component2 = (IntType) 1 << 1,
+            Component3 = (IntType) 1 << 2,
+            Component4 = (IntType) 1 << 3,
+            Component5 = (IntType) 1 << 4,
+            Component6 = (IntType) 1 << 5,
+            Component7 = (IntType) 1 << 6,
+            Component8 = (IntType) 1 << 7,
+            Component9 = (IntType) 1 << 8,
+            Component10 = (IntType) 1 << 9,
+            Component11 = (IntType) 1 << 10,
+            Component12 = (IntType) 1 << 11,
+            Component13 = (IntType) 1 << 12,
+            Component14 = (IntType) 1 << 13,
+            Component15 = (IntType) 1 << 14,
+            Component16 = (IntType) 1 << 15,
+            Component17 = (IntType) 1 << 16,
+            Component18 = (IntType) 1 << 17,
+            Component19 = (IntType) 1 << 18,
+            Component20 = (IntType) 1 << 19,
+            Component21 = (IntType) 1 << 20,
+            Component22 = (IntType) 1 << 21,
+            Component23 = (IntType) 1 << 22,
+            Component24 = (IntType) 1 << 23,
+            Component25 = (IntType) 1 << 24,
+            Component26 = (IntType) 1 << 25,
+            Component27 = (IntType) 1 << 26,
+            Component28 = (IntType) 1 << 27,
+            Component29 = (IntType) 1 << 28,
+            Component30 = (IntType) 1 << 29,
+            Component31 = (IntType) 1 << 30,
+            Component32 = (IntType) 1 << 31,
+            Component33 = (IntType) 1 << 32,
+            Component34 = (IntType) 1 << 33,
+            Component35 = (IntType) 1 << 34,
+            Component36 = (IntType) 1 << 35,
+            Component37 = (IntType) 1 << 36,
+            Component38 = (IntType) 1 << 37,
+            Component39 = (IntType) 1 << 38,
+            Component40 = (IntType) 1 << 39,
+            Component41 = (IntType) 1 << 40,
+            Component42 = (IntType) 1 << 41,
+            Component43 = (IntType) 1 << 42,
+            Component44 = (IntType) 1 << 43,
+            Component45 = (IntType) 1 << 44,
+            Component46 = (IntType) 1 << 45,
+            Component47 = (IntType) 1 << 46,
+            Component48 = (IntType) 1 << 47,
+            Component49 = (IntType) 1 << 48,
+            Component50 = (IntType) 1 << 49,
+            Component51 = (IntType) 1 << 50,
+            Component52 = (IntType) 1 << 51,
+            Component53 = (IntType) 1 << 52,
+            Component54 = (IntType) 1 << 53,
+            Component55 = (IntType) 1 << 54,
+            Component56 = (IntType) 1 << 55,
+            Component57 = (IntType) 1 << 56,
+            Component58 = (IntType) 1 << 57,
+            Component59 = (IntType) 1 << 58,
+            Component60 = (IntType) 1 << 59,
+            Component61 = (IntType) 1 << 60,
+            Component62 = (IntType) 1 << 61,
+            Component63 = (IntType) 1 << 62,
+            Component64 = (IntType) 1 << 63,
         }
 
         private const int NumberOfOperations = 100000;
         private const int NumberOfBits = 8;
 
-        private static readonly int[] WantedBits = new[] {58};
+        private static readonly int[] WantedBits = {58};
 
         private ComponentType[] _enumComponentMasks;
         private ComponentType _wantedEnumMask;
@@ -92,8 +94,8 @@ namespace Benchmark
         private BitMask[] _componentBitMasks;
         private BitMask _wantedBitMask;
 
-        private long[] _intComponentMasks;
-        private long _wantedIntMask;
+        private IntType[] _intComponentMasks;
+        private IntType _wantedIntMask;
 
 
         public BenchmarkImplementation()
@@ -107,7 +109,7 @@ namespace Benchmark
             _wantedEnumMask = ComponentType.None;
             _wantedBitMask = new BitMask(bits: WantedBits);
 
-            _intComponentMasks = new long[NumberOfOperations];
+            _intComponentMasks = new IntType[NumberOfOperations];
             _wantedIntMask = 0;
 
             var random = new Random();
@@ -116,23 +118,23 @@ namespace Benchmark
             {
                 var bit = WantedBits[i];
 
-                _wantedEnumMask |= (ComponentType) (1 << bit);
-                _wantedIntMask |= (1 << bit);
+                _wantedEnumMask |= (ComponentType) ((IntType) 1 << bit);
+                _wantedIntMask |= (IntType) 1 << bit;
             }
 
             for (int i = 0; i < NumberOfOperations; ++i)
             {
                 ComponentType enumComponentMask = ComponentType.None;
                 BitMask componentBitMask = BitMask.None;
-                int intComponentMask = 0;
+                IntType intComponentMask = 0;
 
                 for (int b = 0; b < NumberOfBits; ++b)
                 {
                     var bit = random.Next(0, 31);
 
-                    enumComponentMask |= (ComponentType) (1 << bit);
+                    enumComponentMask |= (ComponentType) ((IntType) 1 << bit);
                     componentBitMask |= new BitMask(bits: new[] {bit});
-                    intComponentMask |= (1 << bit);
+                    intComponentMask |= (IntType) 1 << bit;
                 }
 
                 _enumComponentMasks[i] = enumComponentMask;
